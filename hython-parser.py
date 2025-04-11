c2f6f6e9050c51a83c70b42fc289fa597b40144f
emanuelamaral <amoz_c.p.t@hotmail.com>
2025-04-11 21:35:03 +0100

    Adjusts in Parser to get more generic and flexible

--- hython-parser.py
+++ hython-parser.py
@@ -1,20 +1,32 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
+"""
+=============================================
+                ANÁLISE LÉXICA
+=============================================
+Esta seção define os tokens e regras léxicas para o analisador.
+Os tokens representam os componentes básicos da linguagem.
+"""
+
+
 tokens = (
     'ACTORS', 'CASES', 'EXTENDS', 'INCLUDE', 'INHERIT', 'PACKAGE',
     'LBRACE', 'RBRACE', 'COMMA', 'EQUALS', 'IDENTIFIER', 'GT', 'SLASH',
-    'CASE', 'SIMPLE', 'ACTOR'
+    'CASE', 'SIMPLE', 'ACTOR', 'CLASS', 'COMPONENT', 'ASSOCIATES', 
+    'REALIZES', 'DEPENDS', 'USES', 'INTERFACE'
 )
 
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
-t_COMMA = r','
-t_EQUALS = r'='
-t_GT = r'>'
-t_SLASH = r'/'
-t_ignore = " \t"
+# Tokens simples
+t_LBRACE = r'\{' # Delimitador de início de bloco
+t_RBRACE = r'\}' # Delimitador de fim de bloco
+t_COMMA = r','   # Separador de itens em listas
+t_EQUALS = r'='  # Operador de atribuição
+t_GT = r'>'      # Símbolo maior que (usado em tags)
+t_SLASH = r'/'   # Barra (usada no fechamento de tags)
+t_ignore = " \t" # Caracteres a serem ignorados (espaços e tabs)
 
+# Palavras-chave com tags
 def t_ACTORS(t):
     r'actors'
     return t
@@ -24,160 +36,260 @@ def t_CASES(t):
     return t
 
 def t_SIMPLE(t):
-    r'\<simple\>'
+    r'<simple>'
     return t
 
 def t_CASE(t):
-    r'\<case\>'
+    r'<case>'
     return t
 
 def t_ACTOR(t):
-    r'\<actor\>'
+    r'<actor>'
     return t
 
 def t_EXTENDS(t):
-    r'\<extends\>'
+    r'<extends>'
     return t
 
 def t_INCLUDE(t):
-    r'\<include\>'
+    r'<include>'
     return t
 
 def t_INHERIT(t):
-    r'\<inherit\>'
+    r'<inherit>'
     return t
 
 def t_PACKAGE(t):
-    r'\<package\>'
+    r'<package>'
+    return t
+
+def t_CLASS(t):
+    r'<class>'
+    return t
+
+def t_COMPONENT(t):
+    r'<component>'
+    return t
+
+def t_INTERFACE(t):
+    r'<interface>'
+    return t
+
+def t_USES(t):
+    r'<uses>'
+    return t
+
+def t_REALIZES(t):
+    r'<realizes>'
+    return t
+
+def t_ASSOCIATES(t):
+    r'<associates>'
+    return t
+
+def t_DEPENDS(t):
+    r'<depends>'
     return t
 
 def t_IDENTIFIER(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     return t
 
+# Tratamento de quebras de linha (importante para mensagens de erro)
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+# Tratamento de erros léxicos
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
+    print(f"Caractere ilegal '{t.value[0]}'")
     t.lexer.skip(1)
 
 lexer = lex.lex()
+
+"""
+=============================================
+              ANÁLISE SINTÁTICA
+=============================================
+Esta seção define a gramática da linguagem e como os tokens
+se combinam para formar estruturas válidas.
+"""
 
 def p_program(p):
     '''program : definitions relations'''
     p[0] = {'definitions': p[1], 'relations': p[2]}
 
+    """
+    Um programa completo consiste em:
+    1. Definições (atores e casos de uso)
+    2. Relações entre os elementos
+    """
+
 def p_definitions(p):
     '''definitions : definitions definition
-                   | definition'''
+                  | definition'''
     if len(p) == 2:
         p[0] = [p[1]]
     else:
         p[0] = p[1] + [p[2]]
 
+    """
+    Lista de definições pode ser:
+    1. Uma definição isolada
+    2. Ou várias definições em sequência
+    """
+
 def p_definition(p):
     '''definition : ACTORS EQUALS LBRACE actors RBRACE
-                  | CASES EQUALS LBRACE cases RBRACE'''
+                 | CASES EQUALS LBRACE cases RBRACE'''
     p[0] = (p[1], p[4])
 
+    """
+    Cada definição pode ser:
+    1. Conjunto de atores
+    2. Conjunto de casos de uso
+    """
+
+# Listas de atores e casos de uso
 def p_actors(p):
     '''actors : actors COMMA IDENTIFIER
-              | IDENTIFIER'''
-    if len(p) == 2:
-        p[0] = [p[1]]
-    else:
-        p[0] = p[1] + [p[3]]
-
-def p_cases(p):
-    '''cases : cases COMMA IDENTIFIER
              | IDENTIFIER'''
     if len(p) == 2:
         p[0] = [p[1]]
     else:
         p[0] = p[1] + [p[3]]
 
-def p_relations(p):
-    '''relations : PACKAGE IDENTIFIER relation_list'''
-    p[0] = ('package', p[2], p[3])
+    """
+    Lista de atores pode ser:
+    1. Um único ator
+    2. Vários atores separados por vírgulas
+    """
 
-def p_relation_list(p):
-    '''relation_list : relation_list relation
-                     | relation'''
-    if len(p) == 2:
-        p[0] = [p[1]]
-    else:
-        p[0] = p[1] + [p[2]]
-
-def p_relation(p):
-    '''relation : EXTENDS CASE IDENTIFIER CASE identifier_list SLASH GT
-                | INCLUDE CASE IDENTIFIER CASE identifier_list SLASH GT
-                | INHERIT CASE IDENTIFIER CASE identifier_list SLASH GT
-                | SIMPLE  CASE IDENTIFIER CASE identifier_list SLASH GT 
-                | EXTENDS ACTOR IDENTIFIER CASE identifier_list SLASH GT
-                | INCLUDE ACTOR IDENTIFIER CASE identifier_list SLASH GT
-                | INHERIT ACTOR IDENTIFIER CASE identifier_list SLASH GT
-                | SIMPLE  ACTOR IDENTIFIER CASE identifier_list SLASH GT'''
-    if p[1] == 'simple':
-        p[0] = (p[1], p[3], p[5])
-    else:
-        p[0] = (p[1], p[3], p[5])
-
-def p_identifier_list(p):
-    '''identifier_list : identifier_list COMMA IDENTIFIER
-                       | IDENTIFIER'''
+def p_cases(p):
+    '''cases : cases COMMA IDENTIFIER
+            | IDENTIFIER'''
     if len(p) == 2:
         p[0] = [p[1]]
     else:
         p[0] = p[1] + [p[3]]
 
+    """
+    Lista de casos de uso segue a mesma estrutura dos atores
+    """
+
+def p_relations(p):
+    '''relations : PACKAGE IDENTIFIER relation_list'''
+    p[0] = ('package', p[2], p[3])
+
+    """
+    Bloco de relações deve:
+    1. Começar com tag <package> e nome
+    2. Conter lista de relações
+    """
+
+def p_relation_list(p):
+    '''relation_list : relation_list relation
+                    | relation'''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[2]]
+
+    """
+    Lista de relações pode ser:
+    1. Uma única relação
+    2. Várias relações em sequência
+    """
+    
+
+def p_relation(p):
+    '''relation : relation_type source target_participants SLASH GT'''
+    p[0] = {
+        'type': p[1],
+        'participants': [p[2]] + p[3]
+    }
+    
+    """
+    Define uma relação entre elementos UML
+    Combina o tipo de relação com os participantes (fonte + alvos)
+    """
+
+def p_source(p):
+    '''source : element_type IDENTIFIER'''
+    p[0] = {'type': p[1], 'name': p[2]}
+
+    # Define quem inicia a relação (ex: <actor> Usuário)
+
+def p_target_participants(p):
+    '''target_participants : target_participants element_type IDENTIFIER
+                          | element_type IDENTIFIER'''
+    if len(p) == 3:
+        p[0] = [{'type': p[1], 'name': p[2]}]
+    else:
+        p[0] = p[1] + [{'type': p[2], 'name': p[3]}]
+
+    """
+    Permite vários elementos como destino na relação
+    # Ex: <case> Login, <case> Verificação2FA
+    """
+
+def p_element_type(p):
+    '''element_type : CASE
+                   | ACTOR
+                   | CLASS
+                   | COMPONENT
+                   | INTERFACE'''
+    p[0] = p[1]
+
+    # Tipos de elementos que podem participar das relações
+
+def p_relation_type(p):
+    '''relation_type : EXTENDS
+                    | INCLUDE
+                    | INHERIT
+                    | SIMPLE
+                    | USES
+                    | REALIZES
+                    | ASSOCIATES
+                    | DEPENDS'''
+    p[0] = p[1]
+
+    # Tipos de conexões permitidas entre elementos
+
 def p_error(p):
     if p:
-        print(f"Syntax error at '{p.value}', line {p.lineno}")
+        print(f"Erro sintático em '{p.value}' na linha {p.lineno}")
     else:
-        print("Syntax error at EOF")
+        print("Erro sintático no final do arquivo")
 
 parser = yacc.yacc()
 
 data = """
 actors = {
-    Receptionist,
-    Doctor,
-    Nurse,
-    BillingSpecialist
+    Customer,
+    Admin
 }
 
 cases = {
-    ScheduleAppointment,
-    RegisterPatient,
-    PerformCheckup,
-    PrescribeMedication,
+    BrowseProducts,
+    AddToCart,
+    PlaceOrder,
     ProcessPayment,
-    GenerateInvoice,
-    EmergencyAdmission,
-    RoutineExamination
+    TrackShipment,
+    ManageInventory,
+    ApplyDiscount,
+    InternationalOrder,
+    GuestCheckout
 }
 
-<package> HospitalManagementSystem
-    <simple> <actor> Receptionist <case> ScheduleAppointment />
-    <simple> <actor> Receptionist <case> RegisterPatient />
-    <simple> <actor> Doctor <case> PerformCheckup />
-    <simple> <actor> Doctor <case> PrescribeMedication />
-    <simple> <actor> Nurse <case> EmergencyAdmission />
-    <simple> <actor> BillingSpecialist <case> ProcessPayment />
-    
-    <extends> <case> EmergencyAdmission <case> RegisterPatient />
-    <extends> <case> RoutineExamination <case> PerformCheckup />
-    
-    <include> <case> PrescribeMedication <case> PerformCheckup />
-    <include> <case> GenerateInvoice <case> ProcessPayment />
-    
-    <inherit> <case> EmergencyAdmission <case> RoutineExamination />
-    <inherit> <case> GenerateInvoice <case> ProcessPayment />
-    
-    <extends> <case> RegisterPatient <case> ScheduleAppointment, ProcessPayment />
-    <include> <case> PerformCheckup <case> PrescribeMedication, RoutineExamination />
+<package> ECommerceSystem
+    <simple> <actor> Customer <case> BrowseProducts />
+    <simple> <actor> Customer <case> AddToCart />
+    <include> <case> PlaceOrder <case> AddToCart <case> ProcessPayment />
+    <extends> <case> InternationalOrder <case> PlaceOrder />
+    <inherit> <case> GuestCheckout <case> PlaceOrder />
+    <simple> <actor> Admin <case> ManageInventory />
+    <associates> <class> ShoppingCart <class> Product />
 """
 
 result = parser.parse(data)
--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'ACTOR ACTORS CASE CASES COMMA EQUALS EXTENDS GT IDENTIFIER INCLUDE INHERIT LBRACE PACKAGE RBRACE SIMPLE SLASHprogram : definitions relationsdefinitions : definitions definition\n| definitiondefinition : ACTORS EQUALS LBRACE actors RBRACE\n| CASES EQUALS LBRACE cases RBRACEactors : actors COMMA IDENTIFIER\n| IDENTIFIERcases : cases COMMA IDENTIFIER\n| IDENTIFIERrelations : PACKAGE IDENTIFIER relation_listrelation_list : relation_list relation\n| relationrelation : EXTENDS CASE IDENTIFIER CASE identifier_list SLASH GT\n| INCLUDE CASE IDENTIFIER CASE identifier_list SLASH GT\n| INHERIT CASE IDENTIFIER CASE identifier_list SLASH GT\n| SIMPLE  CASE IDENTIFIER CASE identifier_list SLASH GT \n| EXTENDS ACTOR IDENTIFIER CASE identifier_list SLASH GT\n| INCLUDE ACTOR IDENTIFIER CASE identifier_list SLASH GT\n| INHERIT ACTOR IDENTIFIER CASE identifier_list SLASH GT\n| SIMPLE  ACTOR IDENTIFIER CASE identifier_list SLASH GTidentifier_list : identifier_list COMMA IDENTIFIER\n| IDENTIFIER'
+_lr_signature = 'ACTOR ACTORS ASSOCIATES CASE CASES CLASS COMMA COMPONENT DEPENDS EQUALS EXTENDS GT IDENTIFIER INCLUDE INHERIT INTERFACE LBRACE PACKAGE RBRACE REALIZES SIMPLE SLASH USESprogram : definitions relationsdefinitions : definitions definition\n| definitiondefinition : ACTORS EQUALS LBRACE actors RBRACE\n| CASES EQUALS LBRACE cases RBRACEactors : actors COMMA IDENTIFIER\n| IDENTIFIERcases : cases COMMA IDENTIFIER\n| IDENTIFIERrelations : PACKAGE IDENTIFIER relation_listrelation_list : relation_list relation\n| relationrelation : relation_type source target_participants SLASH GTsource : element_type IDENTIFIERtarget_participants : target_participants element_type IDENTIFIER\n| element_type IDENTIFIERelement_type : CASE\n| ACTOR\n| CLASS\n| COMPONENT\n| INTERFACErelation_type : EXTENDS\n| INCLUDE\n| INHERIT\n| SIMPLE\n| USES\n| REALIZES\n| ASSOCIATES\n| DEPENDS'
     
-_lr_action_items = {'ACTORS':([0,2,3,7,33,35,],[4,4,-3,-2,-4,-5,]),'CASES':([0,2,3,7,33,35,],[5,5,-3,-2,-4,-5,]),'$end':([1,6,14,15,24,73,75,76,77,78,79,80,81,],[0,-1,-10,-12,-11,-13,-17,-14,-18,-15,-19,-16,-20,]),'PACKAGE':([2,3,7,33,35,],[8,-3,-2,-4,-5,]),'EQUALS':([4,5,],[9,10,]),'IDENTIFIER':([8,12,13,25,26,27,28,29,30,31,32,34,36,47,48,49,50,51,52,53,54,65,],[11,21,23,37,38,39,40,41,42,43,44,45,46,55,55,55,55,55,55,55,55,74,]),'LBRACE':([9,10,],[12,13,]),'EXTENDS':([11,14,15,24,73,75,76,77,78,79,80,81,],[16,16,-12,-11,-13,-17,-14,-18,-15,-19,-16,-20,]),'INCLUDE':([11,14,15,24,73,75,76,77,78,79,80,81,],[17,17,-12,-11,-13,-17,-14,-18,-15,-19,-16,-20,]),'INHERIT':([11,14,15,24,73,75,76,77,78,79,80,81,],[18,18,-12,-11,-13,-17,-14,-18,-15,-19,-16,-20,]),'SIMPLE':([11,14,15,24,73,75,76,77,78,79,80,81,],[19,19,-12,-11,-13,-17,-14,-18,-15,-19,-16,-20,]),'CASE':([16,17,18,19,37,38,39,40,41,42,43,44,],[25,27,29,31,47,48,49,50,51,52,53,54,]),'ACTOR':([16,17,18,19,],[26,28,30,32,]),'RBRACE':([20,21,22,23,45,46,],[33,-7,35,-9,-6,-8,]),'COMMA':([20,21,22,23,45,46,55,56,57,58,59,60,61,62,63,74,],[34,-7,36,-9,-6,-8,-22,65,65,65,65,65,65,65,65,-21,]),'SLASH':([55,56,57,58,59,60,61,62,63,74,],[-22,64,66,67,68,69,70,71,72,-21,]),'GT':([64,66,67,68,69,70,71,72,],[73,75,76,77,78,79,80,81,]),}
+_lr_action_items = {'ACTORS':([0,2,3,7,37,39,],[4,4,-3,-2,-4,-5,]),'CASES':([0,2,3,7,37,39,],[5,5,-3,-2,-4,-5,]),'$end':([1,6,14,15,29,49,],[0,-1,-10,-12,-11,-13,]),'PACKAGE':([2,3,7,37,39,],[8,-3,-2,-4,-5,]),'EQUALS':([4,5,],[9,10,]),'IDENTIFIER':([8,12,13,31,32,33,34,35,36,38,40,42,47,],[11,26,28,43,-17,-18,-19,-20,-21,44,45,48,50,]),'LBRACE':([9,10,],[12,13,]),'EXTENDS':([11,14,15,29,49,],[17,17,-12,-11,-13,]),'INCLUDE':([11,14,15,29,49,],[18,18,-12,-11,-13,]),'INHERIT':([11,14,15,29,49,],[19,19,-12,-11,-13,]),'SIMPLE':([11,14,15,29,49,],[20,20,-12,-11,-13,]),'USES':([11,14,15,29,49,],[21,21,-12,-11,-13,]),'REALIZES':([11,14,15,29,49,],[22,22,-12,-11,-13,]),'ASSOCIATES':([11,14,15,29,49,],[23,23,-12,-11,-13,]),'DEPENDS':([11,14,15,29,49,],[24,24,-12,-11,-13,]),'CASE':([16,17,18,19,20,21,22,23,24,30,41,43,48,50,],[32,-22,-23,-24,-25,-26,-27,-28,-29,32,32,-14,-16,-15,]),'ACTOR':([16,17,18,19,20,21,22,23,24,30,41,43,48,50,],[33,-22,-23,-24,-25,-26,-27,-28,-29,33,33,-14,-16,-15,]),'CLASS':([16,17,18,19,20,21,22,23,24,30,41,43,48,50,],[34,-22,-23,-24,-25,-26,-27,-28,-29,34,34,-14,-16,-15,]),'COMPONENT':([16,17,18,19,20,21,22,23,24,30,41,43,48,50,],[35,-22,-23,-24,-25,-26,-27,-28,-29,35,35,-14,-16,-15,]),'INTERFACE':([16,17,18,19,20,21,22,23,24,30,41,43,48,50,],[36,-22,-23,-24,-25,-26,-27,-28,-29,36,36,-14,-16,-15,]),'RBRACE':([25,26,27,28,44,45,],[37,-7,39,-9,-6,-8,]),'COMMA':([25,26,27,28,44,45,],[38,-7,40,-9,-6,-8,]),'SLASH':([41,48,50,],[46,-16,-15,]),'GT':([46,],[49,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'program':([0,],[1,]),'definitions':([0,],[2,]),'definition':([0,2,],[3,7,]),'relations':([2,],[6,]),'relation_list':([11,],[14,]),'relation':([11,14,],[15,24,]),'actors':([12,],[20,]),'cases':([13,],[22,]),'identifier_list':([47,48,49,50,51,52,53,54,],[56,57,58,59,60,61,62,63,]),}
+_lr_goto_items = {'program':([0,],[1,]),'definitions':([0,],[2,]),'definition':([0,2,],[3,7,]),'relations':([2,],[6,]),'relation_list':([11,],[14,]),'relation':([11,14,],[15,29,]),'relation_type':([11,14,],[16,16,]),'actors':([12,],[25,]),'cases':([13,],[27,]),'source':([16,],[30,]),'element_type':([16,30,41,],[31,42,47,]),'target_participants':([30,],[41,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,26 +27,33 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> program","S'",1,None,None,None),
-  ('program -> definitions relations','program',2,'p_program','hython-parser.py',69),
-  ('definitions -> definitions definition','definitions',2,'p_definitions','hython-parser.py',73),
-  ('definitions -> definition','definitions',1,'p_definitions','hython-parser.py',74),
-  ('definition -> ACTORS EQUALS LBRACE actors RBRACE','definition',5,'p_definition','hython-parser.py',81),
-  ('definition -> CASES EQUALS LBRACE cases RBRACE','definition',5,'p_definition','hython-parser.py',82),
-  ('actors -> actors COMMA IDENTIFIER','actors',3,'p_actors','hython-parser.py',86),
-  ('actors -> IDENTIFIER','actors',1,'p_actors','hython-parser.py',87),
-  ('cases -> cases COMMA IDENTIFIER','cases',3,'p_cases','hython-parser.py',94),
-  ('cases -> IDENTIFIER','cases',1,'p_cases','hython-parser.py',95),
-  ('relations -> PACKAGE IDENTIFIER relation_list','relations',3,'p_relations','hython-parser.py',102),
-  ('relation_list -> relation_list relation','relation_list',2,'p_relation_list','hython-parser.py',106),
-  ('relation_list -> relation','relation_list',1,'p_relation_list','hython-parser.py',107),
-  ('relation -> EXTENDS CASE IDENTIFIER CASE identifier_list SLASH GT','relation',7,'p_relation','hython-parser.py',114),
-  ('relation -> INCLUDE CASE IDENTIFIER CASE identifier_list SLASH GT','relation',7,'p_relation','hython-parser.py',115),
-  ('relation -> INHERIT CASE IDENTIFIER CASE identifier_list SLASH GT','relation',7,'p_relation','hython-parser.py',116),
-  ('relation -> SIMPLE CASE IDENTIFIER CASE identifier_list SLASH GT','relation',7,'p_relation','hython-parser.py',117),
-  ('relation -> EXTENDS ACTOR IDENTIFIER CASE identifier_list SLASH GT','relation',7,'p_relation','hython-parser.py',118),
-  ('relation -> INCLUDE ACTOR IDENTIFIER CASE identifier_list SLASH GT','relation',7,'p_relation','hython-parser.py',119),
-  ('relation -> INHERIT ACTOR IDENTIFIER CASE identifier_list SLASH GT','relation',7,'p_relation','hython-parser.py',120),
-  ('relation -> SIMPLE ACTOR IDENTIFIER CASE identifier_list SLASH GT','relation',7,'p_relation','hython-parser.py',121),
-  ('identifier_list -> identifier_list COMMA IDENTIFIER','identifier_list',3,'p_identifier_list','hython-parser.py',128),
-  ('identifier_list -> IDENTIFIER','identifier_list',1,'p_identifier_list','hython-parser.py',129),
+  ('program -> definitions relations','program',2,'p_program','hython-parser.py',108),
+  ('definitions -> definitions definition','definitions',2,'p_definitions','hython-parser.py',112),
+  ('definitions -> definition','definitions',1,'p_definitions','hython-parser.py',113),
+  ('definition -> ACTORS EQUALS LBRACE actors RBRACE','definition',5,'p_definition','hython-parser.py',120),
+  ('definition -> CASES EQUALS LBRACE cases RBRACE','definition',5,'p_definition','hython-parser.py',121),
+  ('actors -> actors COMMA IDENTIFIER','actors',3,'p_actors','hython-parser.py',125),
+  ('actors -> IDENTIFIER','actors',1,'p_actors','hython-parser.py',126),
+  ('cases -> cases COMMA IDENTIFIER','cases',3,'p_cases','hython-parser.py',133),
+  ('cases -> IDENTIFIER','cases',1,'p_cases','hython-parser.py',134),
+  ('relations -> PACKAGE IDENTIFIER relation_list','relations',3,'p_relations','hython-parser.py',141),
+  ('relation_list -> relation_list relation','relation_list',2,'p_relation_list','hython-parser.py',145),
+  ('relation_list -> relation','relation_list',1,'p_relation_list','hython-parser.py',146),
+  ('relation -> relation_type source target_participants SLASH GT','relation',5,'p_relation','hython-parser.py',153),
+  ('source -> element_type IDENTIFIER','source',2,'p_source','hython-parser.py',160),
+  ('target_participants -> target_participants element_type IDENTIFIER','target_participants',3,'p_target_participants','hython-parser.py',164),
+  ('target_participants -> element_type IDENTIFIER','target_participants',2,'p_target_participants','hython-parser.py',165),
+  ('element_type -> CASE','element_type',1,'p_element_type','hython-parser.py',172),
+  ('element_type -> ACTOR','element_type',1,'p_element_type','hython-parser.py',173),
+  ('element_type -> CLASS','element_type',1,'p_element_type','hython-parser.py',174),
+  ('element_type -> COMPONENT','element_type',1,'p_element_type','hython-parser.py',175),
+  ('element_type -> INTERFACE','element_type',1,'p_element_type','hython-parser.py',176),
+  ('relation_type -> EXTENDS','relation_type',1,'p_relation_type','hython-parser.py',180),
+  ('relation_type -> INCLUDE','relation_type',1,'p_relation_type','hython-parser.py',181),
+  ('relation_type -> INHERIT','relation_type',1,'p_relation_type','hython-parser.py',182),
+  ('relation_type -> SIMPLE','relation_type',1,'p_relation_type','hython-parser.py',183),
+  ('relation_type -> USES','relation_type',1,'p_relation_type','hython-parser.py',184),
+  ('relation_type -> REALIZES','relation_type',1,'p_relation_type','hython-parser.py',185),
+  ('relation_type -> ASSOCIATES','relation_type',1,'p_relation_type','hython-parser.py',186),
+  ('relation_type -> DEPENDS','relation_type',1,'p_relation_type','hython-parser.py',187),
 ]
